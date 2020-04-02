9f0005a88a3a876135a6c183f6f7d4ad113b6813
Vladimir-Zaitsev <gaara2113@rambler.ru>
2020-04-02 17:17:40 +0400

    поменял все по бэм,добавил переменные

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,22 +1,22 @@
-var button = document.querySelector(".slider-link_js");
-var modalForm = document.querySelector(".modal-form_js");
-var buttonClose = document.querySelector(".button-close_js");
-var input = document.querySelector(".form-input_js")
+var button = document.querySelector(".sliders__link_js");
+var modalForm = document.querySelector(".popup_js");
+var buttonClose = document.querySelector(".popup-form__button-close_js");
+var input = document.querySelector(".popup-form_js")
 
 
 button.addEventListener("click", function () {
-  modalForm.classList.remove("modal-form_none");
+  modalForm.classList.remove("popup_none");
   input.focus();
 });
 
 buttonClose.addEventListener("click", function() {
-  modalForm.classList.add("modal-form_none");
+  modalForm.classList.add("popup_none");
   button.focus();
 });
 
 window.addEventListener("keydown", function (event) {
-  if (event.code == "Escape" && !modalForm.classList.contains("modal-form_none")) {
-    modalForm.classList.add("modal-form_none");
+  if (event.code == "Escape" && !modalForm.classList.contains("popup_none")) {
+    modalForm.classList.add("popup_none");
     button.focus();        
   }
 });
